736447c161651fda8f1697ee2d0589ccb13bbcfe
REACT10_ThanhHoang <hoangvdtse160556@fpt.edu.vn>
2025-02-05 10:01:05 +0700

    no message

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Homepage = () => {
   return (
-    <section className="banner">
+    <section className="banner p-4">
       <h1>Welcome to Labubu Shop!</h1>
       <p>Your favorite place for all things Labubu!</p>
       <button>Đăng nhập</button>
